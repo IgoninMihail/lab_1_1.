3d8a4c221909fdd431430d70299f438f98429ea2
IgoninMihail <92269370+IgoninMihail@users.noreply.github.com>
2021-10-13 15:48:03 +0200

    Добавьте файлы проекта.

---
+++ lab_1_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double a, y, J, x;
+                Console.WriteLine("Введите a:");
+                a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите y:");
+                y = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите x:");
+                x = double.Parse(Console.ReadLine());
+                J = (Math.Pow(Math.Cos(Math.Pow(a, 3)) / Math.Sin(Math.Pow(a, 3)), 3) + Math.Pow(Math.Atan(a), 2)) / (Math.Sqrt(Math.Pow(y, Math.Log(x))));
+                Console.WriteLine("J=", +J);
+                Console.WriteLine(string.Format("J={0:F2}", J));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Введите корректно переменную");
+            }
+
+
+        }
+    }
+}
